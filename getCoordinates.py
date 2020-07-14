c29039057f3910464e8c84faa33e2fca1b3a0065
Manon-Oreins <62328122+Manon-Oreins@users.noreply.github.com>
2020-07-14 13:08:02 +0200

    Ajout de commentaires

--- getCoordinates.py
+++ getCoordinates.py
@@ -38,26 +38,26 @@ def is_correct(answer,correction):
     
 from inginious import input, feedback, rst
 
-answer = input.get_input("q1")
+answer = input.get_input("q1") #Remplacez tous les 'q1' par l'ID du sous-problème 
 grade = 0
 check = is_coordinates(answer)
 
 if check != True : 
-    feedback.set_problem_result("failed","q1")
+    feedback.set_problem_result("failed","q1") #Ici
     if check == "parenthese" :
-        feedback.set_problem_feedback("Votre réponse doit contenir des parenthèses pour délimiter le point","q1")
+        feedback.set_problem_feedback("Votre réponse doit contenir des parenthèses pour délimiter le point","q1") #Ici
     if check == "lettre" :
-        feedback.set_problem_feedback("Votre réponse ne doit contenir que des chiffres","q1")
+        feedback.set_problem_feedback("Votre réponse ne doit contenir que des chiffres","q1") #Ici
     if check == "separateur":
-        feedback.set_problem_feedback("Les coordonnées doivent être séparées par une virgule","q1")
+        feedback.set_problem_feedback("Les coordonnées doivent être séparées par une virgule","q1") #Ici
     
 elif check == True :
-    if is_correct(answer,"(2,2)") == False : #Remplacez 'correction' par la réponse correcte, si la réponse à la question est (5,6), écrivez "(5,6)"
-        feedback.set_problem_result("failed","q1")
-        feedback.set_problem_feedback("Votre réponse est incorrect","q1")
-    elif is_correct(answer,"(2,2)") == True : #Remplacez 'correction' par la réponse correcte, si la réponse à la question est (5,6), écrivez "(5,6)"
-        feedback.set_problem_result("success","q1")
-        feedback.set_problem_feedback("Bravo!","q1")
+    if is_correct(answer,"(2,2)") == False : #Remplacez '(2,2)' par la réponse correcte, si la réponse à la question est (5,6), écrivez "(5,6)"
+        feedback.set_problem_result("failed","q1") #Ici
+        feedback.set_problem_feedback("Votre réponse est incorrect","q1") #Ici
+    elif is_correct(answer,"(2,2)") == True : #Remplacez '(2,2)' par la réponse correcte, si la réponse à la question est (5,6), écrivez "(5,6)"
+        feedback.set_problem_result("success","q1") #Ici
+        feedback.set_problem_feedback("Bravo!","q1") #Ici
         grade += 100
         
 feedback.set_grade(grade)
